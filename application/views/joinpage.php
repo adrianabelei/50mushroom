a006efd686c15f22adcce99688879eeab7166e44
adrianabelei <adrianabelei01@gmail.com>
2018-03-04 12:01:58 +0100

    joinpage css

--- application/views/joinpage.php
+++ application/views/joinpage.php
@@ -6,21 +6,21 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Log In|Registration</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link href=style.css rel="stylesheet"> 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href=style.css rel="stylesheet"> 
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="stylejoinpage.css" />
     <script src="main.js"></script>
-</head>
+    </head>
 
 
 
 
 <body>
-  <div class="container">
+    <div class="container">
     <div class="col-md-6">
    
     <div id="logbox1">
@@ -31,9 +31,9 @@
         <input name="password" type="password" placeholder="Confirm password" class="input pass"/>
         <input name="email" type="email" placeholder="Email address" class="input pass"/>
         <input type="submit" value="Sign me up!" class="inputButton"/>
-        <div class="text-center">
-            <p>Already have an account? <a href="#" id="login_id">Log In</a></p>
-        </div>
+    <div class="text-center">
+        <p>Already have an account? <a href="#" id="login_id">Log In</a></p>
+    </div>
       </form>
     </div>
    </div>
@@ -46,13 +46,13 @@
         <input name="email" type="email" placeholder="Enter your email" class="input pass"/>
         <input name="password" type="password" placeholder="Enter your password" class="input pass"/>
         <input type="submit" value="Sign me in!" class="inputButton"/>
-        <div class="text-center"">
-                    <p><a href="#" id="">Create an account</a> - <a href="#" id="">Forgot password?</a></p>
-                </div>
+        <div class="text-center">
+        <p><a href="#" id="">Create an account</a> - <a href="#" id="">Forgot password?</a></p>
+    </div>
       </form>
     </div>
     </div>
-  </div>
+    </div>
 
 </body>
 </html>
