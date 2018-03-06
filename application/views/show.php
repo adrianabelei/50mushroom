6a366b9a70fbf56ddaa731d44da25fb84572aad2
muslumgezgin <muslum_gezgin@hotmail.com>
2018-03-06 23:26:23 +0100

    manage to bring post image to show.php

--- application/views/show.php
+++ application/views/show.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Piccolo Theme</title>
+<title>Post</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!-- CSS
@@ -147,7 +147,7 @@
 
             <div class="row">
                 <div class="span6">
-                    <img src="img/gallery/gallery-img-1-full.jpg" class="align-left thumbnail" alt="image">
+                    <img src="<?php echo base_url(); ?>uploads/<?= $results['image'] ?>" class="align-left thumbnail" alt="image">
                 </div>
                 <div class="span6">
                     <h2><?= $results['title'] ?></h2>
