8955a7963563a6e5e6960e3a1d91c52610a418df
abboudmourad <aboudmourad@hotmail.com>
2018-03-07 09:30:54 +0100

    modified

--- application/controllers/AdminProcess.php
+++ application/controllers/AdminProcess.php
@@ -1,21 +1,65 @@
 <?php 
 
-<<<<<<< HEAD
+
 class AdminProcess extends CI_COntroller 
+
 {
+    public function index()
+    {
     
-=======
-class Adminprocess extends CI_COntroller 
-{
-   
->>>>>>> 226347333801f116a963fe7fc7fbd0a0c0e3053a
+            $this->load->view('adminlogin'); 
+    }
+
+    
+//////////////////// login SuperAdmin and Admin////////////////
+    public function login()
+	{
+        $logInfo=$this->input->post(null,true);
+		$this->load->model('Admin_Model');
+        $querySuperAdmin=$this->Admin_Model->superAdmin_checker($logInfo);
+        $queryAdmin=$this->Admin_Model->admin_checker($logInfo);
+        
+         
+		if($querySuperAdmin == true)
+		{
+        $array_items = array(
+            'id' => $querySuperAdmin['id'],
+            'email' => $querySuperAdmin['email']);
+        $this->session->set_userdata($array_items);
+        $this->session->set_userdata('id',$querySuperAdmin['id']);
+        $this->session->set_userdata('email',$querySuperAdmin['email']);
+        $this->load->view('adminpanel/adminpanel');
+          
+		
+        }
+        else if($queryAdmin == true)
+		{
+        $array_items = array(
+            'id' => $queryAdmin['id'],
+            'email' => $queryAdmin['email']);
+        $this->session->set_userdata($array_items);
+        $this->session->set_userdata('id',$queryAdmin['id']);
+        $this->session->set_userdata('email',$queryAdmin['email']);
+        $this->load->view('adminpanel/adminpanel');
+          
+		
+		}
+		else
+		{
+			$errlog='Please insert VALID email address OR password!';
+			$this->load->view('adminpanel/adminlogin', array('errlog' => $errlog));
+		}
+    }
+
+
+//////////////////// logout/////////////////////
+    public function logout()
+    {
+        session_destroy();
+        $this->load->view('adminpanel/adminlogin');
+    }
+
+
+/////////////////// END OF THE CLASS //////////////////////
 }
-
-
-
-
-
-
-
-
 ?>
