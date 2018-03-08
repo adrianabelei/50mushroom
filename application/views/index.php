c899abbbf1ea7857925ce49ccf71b2bd8ad9a3ee
muslumgezgin <muslum_gezgin@hotmail.com>
2018-03-08 17:00:00 +0100

    modified index.php

--- application/views/index.php
+++ application/views/index.php
@@ -224,7 +224,7 @@ $(document).ready(function () {
                     <li  class="span3 gallery-item" data-id="id-1" data-type="illustration">
                         <span class="gallery-hover-4col hidden-phone hidden-tablet">
                             <span class="gallery-icons">
-                                <a href="<?php echo base_url(); ?>assets/img/gallery/gallery-img-1-full.jpg" class="item-zoom-link lightbox" title="Custom Illustration" data-rel="prettyPhoto"></a>
+                                <a href="<?php echo base_url(); ?>uploads/<?= $item['image'] ?>" class="item-zoom-link lightbox" title="Custom Illustration" data-rel="prettyPhoto"></a>
                                 <a href="detailpost/<?= $item['id'] ?>" class="item-details-link"></a>
                             </span>
                         </span>
