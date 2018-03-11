99b366ee267447edf7fdd486c32577f7d259c75f
muslumgezgin <muslum_gezgin@hotmail.com>
2018-03-11 13:16:54 +0100

    dkmekdea

--- application/controllers/CompanyProcess.php
+++ application/controllers/CompanyProcess.php
@@ -6,7 +6,7 @@ class CompanyProcess extends CI_Controller
 {
     public function __construct()
     {
-        // parent::__construct();
+         parent::__construct();
         $this->load->helper("url");
     }
     
